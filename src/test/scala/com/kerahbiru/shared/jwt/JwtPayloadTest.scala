4fb65d3881d78f28a56883214be8e3fb87dd75d4
inmyth <martin.bcu@gmail.com>
2021-03-13 12:37:27 +0900

    fixed tests

--- src/test/scala/com/kerahbiru/shared/jwt/JwtPayloadTest.scala
+++ src/test/scala/com/kerahbiru/shared/jwt/JwtPayloadTest.scala
@@ -1,16 +1,18 @@
 package com.kerahbiru.shared.jwt
 
+import com.kerahbiru.shared.jwt.Country.ID
 import io.circe.parser.decode
+import javafx.concurrent.Worker
 import org.scalatest.flatspec.AnyFlatSpec
 
 import java.util.UUID
 
-class JwtPayloadTest extends AnyFlatSpec{
+class JwtPayloadTest extends AnyFlatSpec {
 
   behavior of "JwtPayload"
 
   val uuid = UUID.randomUUID()
-  def json(uuidString: String, role: String) =
+  def json(uuidString: String, role: String, country: String) =
     s"""
       |{
       |  "iss": "https://www.kerahbiru.com",
@@ -19,22 +21,24 @@ class JwtPayloadTest extends AnyFlatSpec{
       |  "sub": "$uuidString",
       |  "alias": "mbcu",
       |  "role": "$role",
-      |  "plan": "free"
+      |  "country": "$country"
       |}
       |""".stripMargin
 
-
-  it should "ok decoding with correctly formatted json" in{
-    val x = decode[JwtPayload](json(uuid.toString, "worker"))
+  it should "ok decoding with correctly formatted json" in {
+    val x = decode[JwtPayload](json(uuid.toString, Role.worker.entryName, ID.entryName))
     assert(x.isRight)
     x.map(_.sub === uuid)
   }
 
-  it should "ko decoding with unknown role" in{
-    val x = decode[JwtPayload](json(uuid.toString, "zyx"))
+  it should "ko decoding with unknown role" in {
+    val x = decode[JwtPayload](json(uuid.toString, "zyx", ID.entryName))
     assert(x.isLeft)
   }
 
-
+  it should "ko decoding with unknown country" in {
+    val x = decode[JwtPayload](json(uuid.toString, Role.worker.entryName, "XX"))
+    assert(x.isLeft)
+  }
 
 }
