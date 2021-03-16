e385c7844fb46155ef7bb2898db920541c7ef550
inmyth <martin.bcu@gmail.com>
2021-03-16 23:47:58 +0900

    formatting

--- src/test/scala/com/kerahbiru/shared/event/OtpToSmsRequestedTest.scala
+++ src/test/scala/com/kerahbiru/shared/event/OtpToSmsRequestedTest.scala
@@ -10,20 +10,20 @@ class OtpToSmsRequestedTest extends AnyFlatSpec {
 
   behavior of "OtpToSmsRequested"
 
-  val id: UUID    = UUID.randomUUID()
-  val phone       = "+442071838750"
-  val countryCode = "uk"
-  val otp         = "12345"
+  val id: UUID = UUID.randomUUID()
+  val phone    = "+442071838750"
+  val country  = "uk"
+  val otp      = "12345"
 
   it should "ok in encoding to json, decoding as event, decoding the data" in {
-    val otpToSmsRequested = OtpToSmsRequested(id, phone, countryCode, otp)
+    val otpToSmsRequested = OtpToSmsRequested(id, phone, country, otp)
     val json              = otpToSmsRequested.asJson.noSpaces
     val event             = decode[Event](json).toOption.get
     assert(event.id === id)
     assert(event.name === EventName.OtpToSmsRequested)
     val data = decode[OtpToSmsRequested.Data](event.data).toOption.get
     assert(data.phone === phone)
-    assert(data.countryCode === countryCode)
+    assert(data.countryCode === country)
     assert(data.otp === otp)
   }
 
