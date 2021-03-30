23a36b485508face2823c3898bfa7b6b4a3d0b69
inmyth <martin.bcu@gmail.com>
2021-03-30 11:47:22 +0900

    fixed SecureDTO, added Salted, tested

--- src/main/scala/com/kerahbiru/shared/dto/SecureDto.scala
+++ src/main/scala/com/kerahbiru/shared/dto/SecureDto.scala
@@ -7,19 +7,25 @@ import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import java.util.UUID
 import scala.util.Try
 
-case class SecureDto(cipherText: String, salt: UUID) {
+case class SecureDto(cipherText: String) {
   val isSecure: Boolean = true
 }
 
 object SecureDto {
 
-  def apply(cipherText: String): SecureDto = SecureDto(cipherText, UUID.randomUUID())
-  implicit val dec: Decoder[SecureDto]     = deriveDecoder
-  implicit val enc: Encoder[SecureDto]     = deriveEncoder
+  def apply(cipherText: String): SecureDto = new SecureDto(cipherText)
+
+  implicit val dec: Decoder[SecureDto] = deriveDecoder
+  implicit val enc: Encoder[SecureDto] = deriveEncoder
 
   implicit val decodeUuidKey: KeyDecoder[UUID] =
     KeyDecoder.instance(s => Try(UUID.fromString(s)).toOption)
 
   implicit val encodeUuidKey: KeyEncoder[UUID] =
     KeyEncoder.instance(_.toString)
+
+  abstract class Salted {
+    val salt: String
+  }
+
 }
--- src/test/scala/com/kerahbiru/shared/dto/SecureDtoTest.scala
+++ src/test/scala/com/kerahbiru/shared/dto/SecureDtoTest.scala
@@ -1,20 +1,34 @@
 package com.kerahbiru.shared.dto
 
+import com.kerahbiru.shared.dto.SecureDto.Salted
+import com.kerahbiru.shared.jwt.Role
+import io.circe.{Decoder, Encoder, KeyDecoder, KeyEncoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.parser.decode
 import io.circe.syntax.EncoderOps
 import org.scalatest.flatspec.AnyFlatSpec
+import io.circe.generic.auto._
+
+import java.util.UUID
+import scala.util.Try
 
 class SecureDtoTest extends AnyFlatSpec {
 
   behavior of "SecureDto"
 
-  val data = "abc"
-  it should "ok serializing deserializing" in {
-    val x = SecureDto(data)
-    val y = x.asJson.noSpaces
-    val z = decode[SecureDto](y).toOption.get
-    assert(z.cipherText === data)
-    assert(z.salt.toString.nonEmpty)
-  }
+  val salt = "blabla"
+  case class Abc(i: Int, s: String, u: UUID, r: Role, salt: String = salt) extends Salted
 
+  it should "ok serializing deserializing salted object" in {
+    val uuid = UUID.randomUUID()
+    val role = Role.org
+    val i    = 1
+    val s    = "a"
+    val x    = Abc(i, s, uuid, role)
+    val y    = x.asJson.noSpaces
+    val z    = decode[Abc](y).toOption.get
+    assert(z.i === i)
+    assert(z.u === uuid)
+    assert(z.salt === salt)
+  }
 }
